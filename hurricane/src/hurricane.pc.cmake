8395be1cc35c938412a6474026d90bff8f6ed0f6
Christophe Alexandre <alliance-users@asim.lip6.fr>
2007-12-27 16:15:28 +0000

    hurricane configuration

---
+++ hurricane/src/hurricane.pc.cmake
@@ -0,0 +1,10 @@
+prefix=@DEST_DIR@
+exec_prefix=${prefix}
+libdir=${exec_prefix}/lib
+includedir=${exec_prefix}/include
+ 
+Name: hurricane
+Description: Hurricane Libs
+Version: 2.0.b
+Libs: -L${libdir} -lhurricane
+Cflags: -I${includedir}/hurricane
