36af88f8c21a22e1494fdf2e58912bb9da2972ad
The Coriolis Project <alliance-users@asim.lip6.fr>
2008-05-06 13:33:39 +0000

    resolve conflict

--- chamsin/src/tests/AnalogicTest.cpp
+++ chamsin/src/tests/AnalogicTest.cpp
@@ -1,27 +1,16 @@
 #include <iostream>
 
 #include "Warning.h"
+#include "Error.h"
 using namespace Hurricane;
 
 #include "AEnv.h"
 
-#include "TrMos.h"
-using namespace DEVICE;
 
 int main() {
     try {
       cout << "simple analogic test" << endl;
-      //CDataBase* db = CDataBase::create();
-      //Library* workLibrary = Library::create(db->getRootLibrary(), Name("WorkLibrary"));
-      //TrMos* trmos = TrMos::create(workLibrary, Name("MosTr"));
       AEnv::create("toto");
-      trmos->create(Transistor::Polarity::P, true); 
-
-      trmos->setMosLength(3);
-      trmos->setMosWidth(3);
-
-      trmos->generate(5, true, false, 3, 3); 
-
       exit(0);
     } catch (Hurricane::Warning& w) {
         cerr << w.what() << endl;
