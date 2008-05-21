9bdf529dc26d5ac969f6635d9aa8889979218c42
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2008-05-21 22:42:11 +0000

    * ./hurricane/src :    - Complete include re-organisation.        include/hurricane/*.h            : kernel.        include/hurricane/viewer/*.h     : hviewer.        include/hurricane/inspector/*.h  : hinspector.      Includes have been moved into subdirectories of .cpp files (as in        crlcore).    - Now you must include files like this :        #include  <hurricane/Layer.h>        #include  <hurricane/viewer/CellViewer.h>        #include  <hurricane/inspector/HInspector.h>    - Suppressed viewer specific include path in FindHURRICANE.cmake.
     * ./crlcore/src/crlcore :
       - Adapted to new Hurricane include scheme. Corrected bugs in it's own
           include files (noticeably "Utilities.h").

--- hurricane/cmake_modules/FindHURRICANE.cmake
+++ hurricane/cmake_modules/FindHURRICANE.cmake
@@ -6,7 +6,7 @@
 #  HURRICANE_LIBRARIES - The path to where the Hurricane library files are.
 
 
-SET(HURRICANE_INCLUDE_PATH_DESCRIPTION "directory containing the Hurricane include files. E.g /usr/local/include/hurricane or /asim/coriolis/include/hurricane")
+SET(HURRICANE_INCLUDE_PATH_DESCRIPTION "directory containing the Hurricane include files. E.g /usr/local/include or /asim/coriolis/include")
 
 SET(HURRICANE_DIR_MESSAGE "Set the HURRICANE_INCLUDE_DIR cmake cache entry to the ${HURRICANE_INCLUDE_PATH_DESCRIPTION}")
 
@@ -24,29 +24,13 @@ IF(UNIX)
   FIND_PATH(HURRICANE_INCLUDE_PATH NAMES Cell.h PATHS
     # Look in other places.
     ${HURRICANE_DIR_SEARCH}
-    PATH_SUFFIXES include/hurricane
+    PATH_SUFFIXES include
     # Help the user find it if we cannot.
     DOC "The ${HURRICANE_INCLUDE_PATH_DESCRIPTION}"
   )
 
   FIND_LIBRARY(HURRICANE_LIBRARY_PATH
     NAMES hurricane
-    PATHS ${HURRICANE_DIR_SEARCH}
-    PATH_SUFFIXES lib
-    # Help the user find it if we cannot.
-    DOC "The ${HURRICANE_INCLUDE_PATH_DESCRIPTION}"
-  )
-
-  FIND_PATH(HURRICANE_VIEWER_INCLUDE_PATH
-    NAMES CellViewer.h HInspectorWidget.h
-    PATHS ${HURRICANE_DIR_SEARCH}
-    PATH_SUFFIXES include/hurricane
-    # Help the user find it if we cannot.
-    DOC "The ${HURRICANE_INCLUDE_PATH_DESCRIPTION}"
-  )
-
-  FIND_LIBRARY(HURRICANE_VIEWER_LIBRARY_PATH
-    NAMES hviewer 
     PATHS ${HURRICANE_DIR_SEARCH}
     PATH_SUFFIXES lib
     # Help the user find it if we cannot.
@@ -77,17 +61,12 @@ IF(UNIX)
   ENDIF(HURRICANE_LIBRARY_PATH)
   ENDIF(HURRICANE_INCLUDE_PATH)
 
-  IF(HURRICANE_VIEWER_INCLUDE_PATH)
   IF(HURRICANE_VIEWER_LIBRARY_PATH)
     SET(HURRICANE_GRAPHICAL_FOUND "YES")
-    SET(HURRICANE_GRAPHICAL_INCLUDE_DIR
-      ${HURRICANE_VIEWER_INCLUDE_PATH}
-    )
     SET(HURRICANE_GRAPHICAL_LIBRARIES
       ${HURRICANE_VIEWER_LIBRARY_PATH}
     )
   ENDIF(HURRICANE_VIEWER_LIBRARY_PATH)
-  ENDIF(HURRICANE_VIEWER_INCLUDE_PATH)
 
   IF (HURRICANE_INSPECTOR_LIBRARY_PATH)
     SET(HURRICANE_GRAPHICAL_LIBRARIES ${HURRICANE_GRAPHICAL_LIBRARIES} ${HURRICANE_INSPECTOR_LIBRARY_PATH})
