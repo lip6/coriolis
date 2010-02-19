e5a423fbd79c8e92c6e04ba55425cce125d00219
Damien Dupuis <alliance-users@asim.lip6.fr>
2010-02-19 15:25:45 +0000

    New compile.sh script that use cmake, there should be no more problems on Linux (except for svn old version)

--- bootstrap/easyChams/compile.sh
+++ bootstrap/easyChams/compile.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-qmake -project
-case "`uname -s`" in
-  Linux)  qmake;;
-  Darwin) qmake -spec macx-g++;;
-esac
-make -j2 && make clean
+if [ ! -e "./compile.sh" ]; then
+  echo "You must run compile.sh in its own direcotry : ./compile.sh"
+  exit 1
+fi
+
+if [ ! -d "build" ]; then
+  echo "Creating build directory"
+  mkdir build
+fi
+
+cd build && cmake .. && make DESTDIR=.. -j2 install
