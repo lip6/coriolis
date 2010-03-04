c738ac2f6e5cdeabc45f65c94c8924e62ff8454f
The Chams Project <alliance-users@asim.lip6.fr>
2010-03-04 13:36:02 +0000

    New version of compiling script to support 32bits and 64 bits achitectures on the same account (for chams network install)

--- bootstrap/easyChams/compile.sh
+++ bootstrap/easyChams/compile.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+ARCH=`uname -m`
 
 if [ ! -e "./compile.sh" ]; then
   echo "You must run compile.sh in its own direcotry : ./compile.sh"
   exit 1
 fi
 
-if [ ! -d "build" ]; then
+if [ ! -d "$ARCH/build" ]; then
   echo "Creating build directory"
-  mkdir build
+  mkdir -p $ARCH/build
 fi
 
-cd build && cmake .. && make DESTDIR=.. -j2 install
+cd $ARCH/build && cmake ../.. && make DESTDIR=.. -j2 install
