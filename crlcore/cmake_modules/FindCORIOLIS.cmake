863bc7a4c439fa69e2ccfc1b7bdfd1911ac973c9
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2010-03-18 15:32:24 +0000

    * All Tools:      - Change: New structure for the installation & CMake system.          * Tools are now grouped in "projects". There are three projects:            1. - IO: Standalones parsers/drivers (IO_USER_TOP, IO_TOP).            2. - Coriolis: Base & digital tools (CORIOLIS_USER_TOP, CORIOLIS_TOP).            3. - Chams: Analogic tools (CHAMS_USER_TOP, CHAMS_TOP).            Each *project* has a two "TOP" environement variables, for          example: IO_TOP and IO_USER_TOP. Thoses variables are the only          ones useds to locate the tool (CMake modules, headers & libraries).            The local path always takes precedence over the global one.            The localisation process occurs in each tool top CMakeLists.txt          where the macro SETUP_PROJECT_PATH is to be defined. There is no          way to put it in a shared includes file as it's the macro precisely          used to locates the includes... You have to call the macro once for          each project you wants to uses:              SETUP_PROJECT_PATHS(IO)              SETUP_PROJECT_PATHS(CORIOLIS)          * In FindTOOL.cmake, supress the <TOOL>_DIR_SEARCH and uses the          <PROJECT>_DIR_SEARCH instead (example: CORIOLIS_DIR_SEARCH).          * buildCoriolis.py modificated according to the new "TOP" scheme.

--- crlcore/cmake_modules/FindCORIOLIS.cmake
+++ crlcore/cmake_modules/FindCORIOLIS.cmake
@@ -1,19 +1,15 @@
 # - Find the Coriolis includes and libraries.
-# The following variables are set if Coriolis is found.  If CORIOLIS is not
-# found, CORIOLIS_FOUND is set to false.
-#  CORIOLIS_FOUND        - True when the Coriolis include directory is found.
-#  CORIOLIS_INCLUDE_DIR - the path to where the Coriolis include files are.
-#  CORIOLIS_LIBRARIES - The path to where the Coriolis library files are.
-
+# The following variables are set if Coriolis is found.
+# If Coriolis is not found, CRLCORE_FOUND is set to false.
+#  CRLCORE_FOUND       - True when the Coriolis include directory is found.
+#  CRLCORE_INCLUDE_DIR - the path to where the Coriolis include files are.
+#  CRLCORE_LIBRARIES   - The path to where the Coriolis library files are.
 
 SET(CORIOLIS_INCLUDE_PATH_DESCRIPTION "directory containing the Coriolis include files. E.g /usr/local/include/coriolis or /asim/coriolis/include/coriolis")
-
 SET(CORIOLIS_DIR_MESSAGE "Set the CORIOLIS_INCLUDE_DIR cmake cache entry to the ${CORIOLIS_INCLUDE_PATH_DESCRIPTION}")
 
 # don't even bother under WIN32
 IF(UNIX)
-
-  SET(CORIOLIS_DIR_SEARCH $ENV{CORIOLIS_TOP} $ENV{HURRICANE_TOP})
   #
   # Look for an installation.
   #
