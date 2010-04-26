25e47d7ea09b120e091b896eb20c4cbced6405ad
Jean-Manuel Caba <alliance-users@asim.lip6.fr>
2010-04-26 14:11:09 +0000

    add copyright note

--- crlcore/src/ccore/openaccess/OAWrapper.h
+++ crlcore/src/ccore/openaccess/OAWrapper.h
@@ -1,3 +1,10 @@
+// x-----------------------------------------------------------------x
+// |  This file is part of the hurricaneAMS Software.                |
+// |  Copyright (c) UPMC/LIP6 2008-2010, All Rights Reserved         |
+// | =============================================================== |
+// |  Author      :                 Chistophe Alexandre              |
+// |  E-mail      :   Christophe.Alexandre@asim.lip6.fr              |
+// x-----------------------------------------------------------------x
 #ifndef  __OA_WRAPPER_H__
 #define  __OA_WRAPPER_H__
 
