ad4c9c855c7908464a9c14b18aa6a077bb1d5861
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2010-05-19 14:31:14 +0000

    * All tools:     - Change: Adds ${LIB_SUFFIX} to all library search paths.

--- solstice/cmake_modules/FindSOLSTICE.cmake
+++ solstice/cmake_modules/FindSOLSTICE.cmake
@@ -26,7 +26,7 @@ IF(UNIX)
   FIND_LIBRARY(SOLSTICE_LIBRARY_PATH
     NAMES solstice
     PATHS ${CORIOLIS_DIR_SEARCH}
-    PATH_SUFFIXES lib
+    PATH_SUFFIXES lib${LIB_SUFFIX}
     # Help the user find it if we cannot.
     DOC "The ${SOLSTICE_INCLUDE_PATH_DESCRIPTION}"
   )
