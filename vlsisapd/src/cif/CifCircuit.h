ff40851987c11802a9240025e182be45266b05fd
Damien Dupuis <alliance-users@asim.lip6.fr>
2010-06-03 14:02:24 +0000

    Copy cif directory (no more svn move for me)

---
+++ vlsisapd/src/cif/CifCircuit.h
@@ -0,0 +1,23 @@
+#ifndef __CIF_CIRCUIT_H
+#define __CIF_CIRCUIT_H
+
+#include <vector>
+
+namespace vlsisapd {
+class CifPolygon;
+class CifCircuit {
+    public:
+        CifCircuit(string name, string unit, double scale);
+
+        bool addPolygon ( CifPolygon* );
+        bool write ( string );
+
+    private:
+        string _name;
+        string _unit;
+        double _scale;
+
+        std::vector<CifPolygon*> _polygons;
+};
+} // namespace
+#endif
