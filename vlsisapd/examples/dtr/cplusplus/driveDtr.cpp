d0b2c0c6eb81f682be7ed6c0f9479f1d060b5f1c
Damien Dupuis <alliance-users@asim.lip6.fr>
2010-06-15 11:29:44 +0000

    Corrections due to addRule signature change

--- vlsisapd/examples/dtr/cplusplus/driveDtr.cpp
+++ vlsisapd/examples/dtr/cplusplus/driveDtr.cpp
@@ -8,14 +8,14 @@ using namespace std;
 int main(int argc, char * argv[]) {
     DTR::Techno* techno = new DTR::Techno(DTR::Name("MyTech"), DTR::Name("micro"));
 
-    techno->addRule (DTR::Name("transistorMinL"), 0.1 , DTR::Name("0.10"), DTR::Name("ref1"));
-    techno->addRule (DTR::Name("transistorMinW"), 0.2 , DTR::Name("0.20"), DTR::Name("ref2"));
-    techno->addRule (DTR::Name("minWidth")      , 0.15, DTR::Name("0.15"), DTR::Name("ref3"), DTR::Name("metal1"));
-    techno->addRule (DTR::Name("minSpacing")    , 0.2 , DTR::Name("0.20"), DTR::Name("ref4"), DTR::Name("metal1"));
-    techno->addRule (DTR::Name("minSpacing")    , 0.1 , DTR::Name("0.10"), DTR::Name("ref5"), DTR::Name("active"), DTR::Name("poly"));
-    techno->addARule(DTR::Name("minExtension")  , 0.2 , DTR::Name("0.20"), DTR::Name("ref6"), DTR::Name("poly")  , DTR::Name("active"));
+    techno->addRule (DTR::Name("transistorMinL"), 0.1 , DTR::Name("ref1"));
+    techno->addRule (DTR::Name("transistorMinW"), 0.2 , DTR::Name("ref2"));
+    techno->addRule (DTR::Name("minWidth")      , 0.15, DTR::Name("ref3"), DTR::Name("metal1"));
+    techno->addRule (DTR::Name("minSpacing")    , 0.2 , DTR::Name("ref4"), DTR::Name("metal1"));
+    techno->addRule (DTR::Name("minSpacing")    , 0.1 , DTR::Name("ref5"), DTR::Name("active"), DTR::Name("poly"));
+    techno->addARule(DTR::Name("minExtension")  , 0.2 , DTR::Name("ref6"), DTR::Name("poly")  , DTR::Name("active"));
     
-    DTR::Rule* rule = techno->addRule(DTR::Name("minArea"), 0.1, DTR::Name("0.10"), DTR::Name("ref7"), DTR::Name("metal1"));
+    DTR::Rule* rule = techno->addRule(DTR::Name("minArea"), 0.1, DTR::Name("ref7"), DTR::Name("metal1"));
     rule->setType(DTR::Name("area"));
 
     techno->writeToFile("./out.dtr.xml");
