91a82cdbfe39c907dcc5bb5607959594905fc8ad
Damien Dupuis <alliance-users@asim.lip6.fr>
2010-07-14 09:21:39 +0000

    Bug correction : when creating a python module do not include .cpp files that are not python definition since corresponding .o files will be added twice (as .o and in lib.so)
        This has been tested on Mac os 10.6 and linux RHEL 32bits.

--- vlsisapd/src/dtr/src/PyDtr.cpp
+++ vlsisapd/src/dtr/src/PyDtr.cpp
@@ -23,7 +23,7 @@ BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(addRule_overloads, addRule, 3, 5);
 BOOST_PYTHON_MODULE(pyDTR) {
     // class DTR::Name
     class_<Name>("Name", init<std::string>())
-        .def("getString", &Name::getString, return_value_policy<copy_const_reference>()) // return_value_policy because this method return a refenrce on string
+        .def("getString", &Name::getString, return_value_policy<copy_const_reference>()) // return_value_policy because this method return a reference on string
         .def(self == self)
         .def(self == std::string())
         .def(self != self)
