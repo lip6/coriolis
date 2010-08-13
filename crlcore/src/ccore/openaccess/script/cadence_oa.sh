ce19e896aaacca7196ab692531b63fc659889246
Jean-Manuel Caba <alliance-users@asim.lip6.fr>
2010-08-13 00:08:16 +0000

    + testing on 32 and 64 bits x86 machines  + change cmake OpenAccess macro we need 2 env variables :     OA_LIB_DIR where are the oa libs     OA_INCLUDE_DIR where are the oa headers    this way different headers version can be tested versus different OA compiled libs

--- crlcore/src/ccore/openaccess/script/cadence_oa.sh
+++ crlcore/src/ccore/openaccess/script/cadence_oa.sh
@@ -1,36 +1,49 @@
 #!/bin/sh
 # 
-# we assume that the directory pointed by OPENACCESS_TOP is like in other project of the lab
-# to do so we should have :
-#  - included symbolic link to bin data lib and license directories from cadence OA
-#  - included doc and example from si2.org OA
-#  - included a symbolic link to an installation of headers from si2.org OA
+# we assume we work on linux for 32bit or 64 bits x86.
 # 
 # this script set environment variable for crlcore compilation with cadence OA
-# with these hypothesis
-#
-#OA_VERSION=oa_v22.04.057
-#ARCH=linux_rhel40_64
+# first we set OPENACCESS_LIB to the oa directory containing the OA version we want to use
+# second we set OPENACCESS_INCLUDE to where the OA include we want to use are
+# third we set the OA_PLUGIN_PATH and systems PATHS so the plugins and shared lib will load.
 #
 
-export OPENACCESS_TOP=$TEMP/CADENCE_OA
+# use oa from virtuoso
+OA_VERSION=oa_v22.04.057
+# get OPUS_TOP and tools
+# from virtuoso editor (needed)
+source ~opus/bin/IC613.sh
 
-#get OPUS_TOP and tools
-#from virtuoso editor
-source $TEMP/IC613.sh
+# get encouter router too if possible
+if test -f ~opus/bin/EDI910.sh
+then
+    source ~opus/bin/EDI910.sh
+fi
+
+
+ARCH=$(uname -i)
+
+OPENACCESS_TOP=$OPUS_TOP/$OA_VERSION
 
 # we need oa + specific cadence libs ...
-export OA_LIB_PATH=$OPENACCESS_TOP/lib:$OPUS_TOP/tools.lnx86/lib/64bit
-echo $OA_LIB_PATH
+case "$ARCH" in
+    "x86_64")
+	export OA_LIB_DIR=$OPENACCESS_TOP/lib/linux_rhel40_64/opt
+	export LD_LIBRARY_PATH=$OA_LIB_DIR:$OPUS_TOP/tools.lnx86/lib/64bit:$LD_LIBRARY_PATH
+	;;
+    *)
+	export OA_LIB_DIR=$OPENACCESS_TOP/lib/linux_rhel40_32/opt
+	export LD_LIBRARY_PATH=$OA_LIB_DIR:$OPUS_TOP/tools.lnx86/lib:$LD_LIBRARY_PATH
+	;;
+esac
 
-export OA_INCLUDE_PATH=$OPENACCESS_TOP/include
-echo $OA_INCLUDE_PATH
+export PATH=$PATH:$OPENACCESS_TOP/bin
 
-export LD_LIBRARY_PATH=$OA_LIB_PATH:$LD_LIBRARY_PATH
+# for plugins .plg files
+export OA_PLUGIN_PATH=$OPENACCESS_TOP/data
 
-#for plugins .plg files
-export OA_PLUGIN_PATH=$OPENACCESS_TOP/data/plugins
-
-#get encouter router too
-source $TEMP/EDI910.sh
-
+# where the si2 hearders have been installed
+# note that $TEMP is /dsk/l1/misc/$(USER) directory
+export OA_INCLUDE_DIR=$TEMP/OA_HEADER
+#si on utilise OA de si2 on aura
+#export OPENACCESS_INCLUDE=$OPENACCESS_TOP/include
