d53d1f53b04e13aca36d447181fd0a1570130a1d
Jean-Manuel Caba <alliance-users@asim.lip6.fr>
2010-08-23 13:21:30 +0000

    oops ...

--- crlcore/src/ccore/OAParserDriver.cpp
+++ crlcore/src/ccore/OAParserDriver.cpp
@@ -11,13 +11,14 @@ namespace CRL {
     OADriver::OADriver(Cell* cell) : _cell(cell) {}
     
     void OADriver::save(const std::string& filePath) {
-    //CRL::OpenAccess::oaDriver(filePath, _cell);
+        CRL::OpenAccess::oaDriver(filePath, _cell);
     }
 
     OAParser::OAParser(const std::string& cellLibPath,const std::string& cellLibName,
                        const std::string& techLibPath,const std::string& techLibName) 
         : _cellLibPath(cellLibPath), _cellLibName(cellLibName), 
-          _techLibPath(techLibPath), _techLibName(techLibName) {}
+          _techLibPath(techLibPath), _techLibName(techLibName) {
+    }
     
     Cell* OAParser::open(const std::string& cellName) {
         return CRL::OpenAccess::oaCellParser(_cellLibPath, _cellLibName, cellName,
