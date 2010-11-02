578877d8d756ec25bff5df762926cc383993bc56
Sophie Belloeil <alliance-users@asim.lip6.fr>
2010-11-02 11:02:44 +0000

    VlsiSAPD is now G++ version 4.4 compliant !

--- vlsisapd/src/bookshelf/src/Exception.cpp
+++ vlsisapd/src/bookshelf/src/Exception.cpp
@@ -24,6 +24,7 @@
 
 
 #include  <cstdarg>
+#include  <cstdio>
 #include  "vlsisapd/bookshelf/Exception.h"
 
 
--- vlsisapd/src/liberty/src/Pin.cpp
+++ vlsisapd/src/liberty/src/Pin.cpp
@@ -1,3 +1,5 @@
+#include<cstdlib>
+
 #include<iostream>
 #include<fstream>
 #include<sstream>
