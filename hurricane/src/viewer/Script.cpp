67af923d4f22c5b73cef7b8608598f71bae068cc
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2011-02-02 12:23:43 +0000

    * <All Tools>/CMakeLists.txt:     - Bug: During the packaging stage, DESTDIR must be appended to the         pathes prepended to CMAKE_MODULE_PATH.

--- hurricane/src/viewer/Script.cpp
+++ hurricane/src/viewer/Script.cpp
@@ -1,41 +1,26 @@
 
 // -*- C++ -*-
 //
-// Copyright (c) BULL S.A. 2000-2010, All Rights Reserved
-//
-// This file is part of Hurricane.
-//
-// Hurricane is free software: you can redistribute it  and/or  modify
-// it under the terms of the GNU  Lesser  General  Public  License  as
-// published by the Free Software Foundation, either version 3 of  the
-// License, or (at your option) any later version.
-//
-// Hurricane is distributed in the hope that it will  be  useful,  but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHAN-
-// TABILITY or FITNESS FOR A PARTICULAR PURPOSE. See  the  Lesser  GNU
-// General Public License for more details.
-//
-// You should have received a copy of the Lesser  GNU  General  Public
-// License along with Hurricane. If not, see
-//                                     <http://www.gnu.org/licenses/>.
+// This file is part of the Coriolis Software.
+// Copyright (c) UPMC/LIP6 2008-2011, All Rights Reserved
 //
 // ===================================================================
 //
 // $Id$
 //
-// x-----------------------------------------------------------------x
+// +-----------------------------------------------------------------+ 
 // |                                                                 |
-// |                  H U R R I C A N E                              |
+// |                   C O R I O L I S                               |
 // |     V L S I   B a c k e n d   D a t a - B a s e                 |
 // |                                                                 |
-// |  Author      :                    Jean-Paul Chaput              |
-// |  E-mail      :            Jean-Paul.Chaput@lip6.fr              |
+// |  Author      :                    Jean-Paul CHAPUT              |
+// |  E-mail      :       Jean-Paul.Chaput@asim.lip6.fr              |
 // | =============================================================== |
-// |  C++ Module  :  "./Script.cpp"                                  |
+// |  C++ Module  :       "./hurricane/viewer/Script.cpp"            |
 // | *************************************************************** |
 // |  U p d a t e s                                                  |
 // |                                                                 |
-// x-----------------------------------------------------------------x
+// +-----------------------------------------------------------------+
 
 
 #include  <sstream>
