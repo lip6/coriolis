d8e82643e6ce8590be9ba41c294b9ccd36fbeef1
Damien Dupuis <alliance-users@asim.lip6.fr>
2011-05-10 14:25:49 +0000

    Adding Boost::Python wrapping

--- vlsisapd/src/spice/src/vlsisapd/spice/Subckt.h
+++ vlsisapd/src/spice/src/vlsisapd/spice/Subckt.h
@@ -20,20 +20,20 @@ class Subckt {
     Subckt(std::string name): _name(name), _interfaces(), _instances(), _parameters() {}
     ~Subckt() {}
 
-    inline std::string                         getName();
-    inline std::vector<std::string>&           getInterfaces();
-    inline std::vector<Instance*>&             getInstances();
-    inline std::map<std::string, std::string>& getParameters();
+    inline const std::string                         getName();
+    inline const std::vector<std::string>&           getInterfaces();
+    inline const std::vector<Instance*>&             getInstances();
+    inline const std::map<std::string, std::string>& getParameters();
 
     inline void addInterface(std::string);
     inline void addInstance (Instance*);
     
     void addParameter(std::string, std::string);
 };
-inline std::string                         Subckt::getName()       { return _name;       }
-inline std::vector<std::string>&           Subckt::getInterfaces() { return _interfaces; }
-inline std::vector<Instance*>&             Subckt::getInstances()  { return _instances;  }
-inline std::map<std::string, std::string>& Subckt::getParameters() { return _parameters; }
+inline const std::string                         Subckt::getName()       { return _name;       }
+inline const std::vector<std::string>&           Subckt::getInterfaces() { return _interfaces; }
+inline const std::vector<Instance*>&             Subckt::getInstances()  { return _instances;  }
+inline const std::map<std::string, std::string>& Subckt::getParameters() { return _parameters; }
 
 inline void Subckt::addInterface(std::string name) { _interfaces.push_back(name); }
 inline void Subckt::addInstance(Instance* inst)    { _instances.push_back(inst);  }
