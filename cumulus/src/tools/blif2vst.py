78823b17c155e563eff815a54accd8573cc383bc
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2020-11-27 18:47:19 +0100

    Transfer of blif2vst.py & yosys.py from alliance-check-toolkit.

--- cumulus/src/tools/blif2vst.py
+++ cumulus/src/tools/blif2vst.py
@@ -50,19 +50,23 @@ def renameNMigenUniquify ( topCell ):
 if __name__ == '__main__':
 
     parser = optparse.OptionParser()
-    parser.add_option( '-c', '--cell'  , type='string',                      dest='cellName'     , help='The name of the BLIF to convert, without extension.')
-    parser.add_option( '-v', '--verbose'              , action='store_true', dest='verbose'      , help='First level of verbosity.')
-    parser.add_option( '-V', '--very-verbose'         , action='store_true', dest='veryVerbose'  , help='Second level of verbosity.')
-    parser.add_option(       '--vst-use-concat'       , action='store_true', dest='vstUseConcat' , help='The VST driver will use "&" (concat) in PORT MAP.')
+    parser.add_option( '-c', '--cell'            , type='string'      , dest='cellName'      , help='The name of the BLIF to convert, without extension.')
+    parser.add_option( '-v', '--verbose'         , action='store_true', dest='verbose'       , help='First level of verbosity.')
+    parser.add_option( '-V', '--very-verbose'    , action='store_true', dest='veryVerbose'   , help='Second level of verbosity.')
+    parser.add_option(       '--vst-use-concat'  , action='store_true', dest='vstUseConcat'  , help='The VST driver will use "&" (concat) in PORT MAP.')
+    parser.add_option(       '--vst-no-lowercase', action='store_true', dest='vstNoLowerCase', help='The VST will keep the case of all identifiers.')
+    parser.add_option(       '--vst-no-linkage'  , action='store_true', dest='vstNoLinkage'  , help='Undefined direction will be set to "in" instead of "linkage".')
     (options, args) = parser.parse_args()
     
-    views    = CRL.Catalog.State.Logical
+    views = CRL.Catalog.State.Logical
     if options.verbose:
         Cfg.getParamBool('misc.verboseLevel1').setBool(True)
     if options.veryVerbose:
         Cfg.getParamBool('misc.verboseLevel1').setBool(True)
         Cfg.getParamBool('misc.verboseLevel2').setBool(True)
-    if options.vstUseConcat: views    |= CRL.Catalog.State.VstUseConcat
+    if options.vstUseConcat:   views |= CRL.Catalog.State.VstUseConcat
+    if options.vstNoLowerCase: views |= CRL.Catalog.State.VstNoLowerCase
+    if options.vstNoLinkage:   views |= CRL.Catalog.State.VstNoLinkage
       
     cell = CRL.Blif.load( options.cellName )
     if cell.getName() == 'top':
