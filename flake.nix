ffdff0b77a71879979e11605310e523ae7646367
Las Safin <me@las.rs>
2021-08-27 16:44:57 +0000

    oroshi

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
         "vlsisapd" "lefdef" "bootstrap" "hurricane" "crlcore"
         "cumulus" "flute" "etesian" "anabatic" "coloquinte"
         "katana" "bora" "equinox" "knik" "katabatic" "ispd"
-        "karakaze" "kite" "nimbus" "metis" "mauka"
+        "karakaze" "kite" "nimbus" "metis" "mauka" "oroshi"
       ];
 
     in
---
+++ nix/oroshi.nix
@@ -0,0 +1,21 @@
+{ version, meta }:
+
+{ lib, stdenv, cmake, ninja, python2, qt4, coriolis-crlcore
+, coriolis-bootstrap, coriolis-vlsisapd, coriolis-hurricane
+, doxygen, boost }:
+
+let boostWithPython = boost.override { enablePython = true; python = python2; }; in
+
+stdenv.mkDerivation {
+  pname = "coriolis-oroshi";
+
+  src = ../oroshi;
+
+  buildInputs = [
+    python2 coriolis-bootstrap coriolis-vlsisapd
+    coriolis-crlcore coriolis-hurricane qt4 boostWithPython
+  ];
+  nativeBuildInputs = [ cmake ninja doxygen ];
+
+  inherit version meta;
+}
