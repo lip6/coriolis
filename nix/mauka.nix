2c4f143e15a53327a589e9e1a8345dffa03d6d75
Las Safin <me@las.rs>
2021-08-27 16:43:24 +0000

    metis, mauka

---
+++ nix/mauka.nix
@@ -0,0 +1,24 @@
+{ version, meta }:
+
+{ lib, stdenv, cmake, ninja, python2, qt4, coriolis-crlcore
+, coriolis-bootstrap, coriolis-vlsisapd, coriolis-hurricane
+, coriolis-nimbus, coriolis-metis, doxygen, boost }:
+
+let boostWithPython = boost.override { enablePython = true; python = python2; }; in
+
+stdenv.mkDerivation {
+  pname = "coriolis-mauka";
+
+  src = ../mauka;
+
+  buildInputs = [
+    python2 coriolis-bootstrap coriolis-vlsisapd coriolis-nimbus
+    coriolis-crlcore coriolis-hurricane qt4 boostWithPython
+    coriolis-metis
+  ];
+  nativeBuildInputs = [ cmake ninja doxygen ];
+
+  inherit version;
+
+  meta = meta // { broken = true; };
+}
---
+++ nix/metis.nix
@@ -0,0 +1,23 @@
+{ version, meta }:
+
+{ lib, stdenv, cmake, ninja, python2, qt4, coriolis-crlcore
+, coriolis-bootstrap, coriolis-vlsisapd, coriolis-hurricane
+, coriolis-nimbus, doxygen, boost }:
+
+let boostWithPython = boost.override { enablePython = true; python = python2; }; in
+
+stdenv.mkDerivation {
+  pname = "coriolis-metis";
+
+  src = ../metis;
+
+  buildInputs = [
+    python2 coriolis-bootstrap coriolis-vlsisapd coriolis-nimbus
+    coriolis-crlcore coriolis-hurricane qt4 boostWithPython
+  ];
+  nativeBuildInputs = [ cmake ninja doxygen ];
+
+  inherit version;
+
+  meta = meta // { broken = true; };
+}
