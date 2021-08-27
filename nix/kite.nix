b6336a02493dbfb72b5f417ba2d47a847e804bfc
Las Safin <me@las.rs>
2021-08-27 16:36:55 +0000

    kite

---
+++ nix/kite.nix
@@ -0,0 +1,24 @@
+{ version, meta }:
+
+{ lib, stdenv, cmake, ninja, python2, boost
+, coriolis-lefdef, coriolis-knik, coriolis-katabatic
+, coriolis-bootstrap, coriolis-vlsisapd, coriolis-hurricane
+, coriolis-crlcore, qt4, coriolis-flute, libxml2 }:
+
+let boostWithPython = boost.override { enablePython = true; python = python2; }; in
+
+stdenv.mkDerivation {
+  pname = "coriolis-kite";
+
+  src = ../kite;
+
+  buildInputs = [
+    python2 boostWithPython coriolis-bootstrap
+    coriolis-vlsisapd coriolis-hurricane coriolis-crlcore
+    coriolis-katabatic qt4 coriolis-flute coriolis-knik libxml2
+    coriolis-lefdef
+  ];
+  nativeBuildInputs = [ cmake ninja ];
+
+  inherit version meta;
+}
