9110d58ef253cc1961d3f1e4432300b6316b0caf
Las Safin <me@las.rs>
2021-08-27 15:07:38 +0000

    equinox

---
+++ nix/equinox.nix
@@ -0,0 +1,22 @@
+{ version, meta }:
+
+{ lib, stdenv, cmake, ninja, python2, boost
+, coriolis-bootstrap, coriolis-vlsisapd, coriolis-hurricane
+, coriolis-crlcore, qt4 }:
+
+let boostWithPython = boost.override { enablePython = true; python = python2; }; in
+
+stdenv.mkDerivation {
+  pname = "coriolis-equinox";
+
+  src = ../equinox;
+
+  buildInputs = [
+    python2 boostWithPython coriolis-bootstrap
+    coriolis-vlsisapd coriolis-hurricane coriolis-crlcore
+    qt4
+  ];
+  nativeBuildInputs = [ cmake ninja ];
+
+  inherit version meta;
+}
