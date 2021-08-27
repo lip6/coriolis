99cf78f58066f0e0072dcf1cc28f03a85181c16d
Las Safin <me@las.rs>
2021-08-27 15:27:53 +0000

    ispd work

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
       components = [
         "vlsisapd" "lefdef" "bootstrap" "hurricane" "crlcore"
         "cumulus" "flute" "etesian" "anabatic" "coloquinte"
-        "katana" "bora" "equinox" "knik" "katabatic"
+        "katana" "bora" "equinox" "knik" "katabatic" "ispd"
       ];
 
     in
---
+++ nix/ispd.nix
@@ -0,0 +1,24 @@
+{ version, meta }:
+
+{ lib, stdenv, cmake, ninja, python2, boost
+, coriolis-bootstrap, coriolis-vlsisapd, coriolis-hurricane
+, coriolis-crlcore, coriolis-flute, coriolis-knik
+, coriolis-lefdef, coriolis-katabatic, libxml2, qt4 }:
+
+let boostWithPython = boost.override { enablePython = true; python = python2; }; in
+
+stdenv.mkDerivation {
+  pname = "coriolis-ispd";
+
+  src = ../ispd;
+
+  buildInputs = [
+    python2 boostWithPython coriolis-bootstrap libxml2
+    coriolis-vlsisapd coriolis-hurricane coriolis-crlcore
+    coriolis-flute coriolis-knik coriolis-lefdef
+    coriolis-katabatic qt4
+  ];
+  nativeBuildInputs = [ cmake ninja ];
+
+  inherit version meta;
+}
