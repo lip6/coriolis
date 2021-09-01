227a8f1721de3900f39d25af998a2b989a6bb7c2
Las Safin <me@las.rs>
2021-09-01 10:03:41 +0000

    Set CORIOLIS_TOP for crlcore check

--- nix/crlcore.nix
+++ nix/crlcore.nix
@@ -7,13 +7,17 @@ let pkg =
     src = ../crlcore;
     buildInputs = [ libxml2 qt4 coriolis-lefdef coriolis-vlsisapd coriolis-hurricane python2Packages.pyqt4 ];
     nativeBuildInputs = [ bison flex doxygen python2Packages.pyqt4 ];
+    postInstall = ''
+      ln -s crlcore/helpers $out/${python2Packages.python.sitePackages}/helpers
+
+      # for import check
+      mkdir -p /build/coriolistop/etc/coriolis2
+      export CORIOLIS_TOP=/build/coriolistop
+    '';
     pythonImportsCheck = [
       "CRL" "Constant" "helpers.io" "helpers.technology"
       "helpers.utils" "helpers.analogtechno" "helpers" "helpers.overlay"
     ];
-    postInstall = ''
-      ln -s crlcore/helpers $out/${python2Packages.python.sitePackages}/helpers
-    '';
     meta.license = lib.licenses.gpl2Plus;
   };
 in generic pkg
