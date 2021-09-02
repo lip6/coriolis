979f97df2f5017cffbf5be0bffdac6db0309b881
Las Safin <me@las.rs>
2021-09-02 09:54:51 +0000

    Include yosys

--- nix/alliance-check-toolkit.nix
+++ nix/alliance-check-toolkit.nix
@@ -1,12 +1,14 @@
 { alliance-src, ... }:
 
-{ stdenv, coriolis-unicorn }:
+{ stdenv, coriolis-unicorn, yosys }:
 stdenv.mkDerivation {
   pname = "alliance-check-toolkit";
   version = builtins.substring 0 8 alliance-src.lastModifiedDate;
   src = alliance-src;
 
-  nativeBuildInputs = [ coriolis-unicorn ];
+  nativeBuildInputs = [ coriolis-unicorn yosys ];
+
+  YOSYS_TOP = "${yosys}";
 
   configurePhase = "true";
   buildPhase = ''
