4f266c04eb02bd80e6aa0c7ce29b947ac6f0a550
Las Safin <me@las.rs>
2021-09-02 22:39:29 +0000

    Get alliance check toolkit to fail

--- nix/alliance-check-toolkit.nix
+++ nix/alliance-check-toolkit.nix
@@ -1,14 +1,19 @@
 { alliance-src, ... }:
 
-{ stdenv, coriolis-unicorn, yosys }:
+{ stdenv, coriolis-unicorn, alliance, yosys }:
 stdenv.mkDerivation {
   pname = "alliance-check-toolkit";
   version = builtins.substring 0 8 alliance-src.lastModifiedDate;
   src = alliance-src;
 
-  nativeBuildInputs = [ coriolis-unicorn yosys ];
+  nativeBuildInputs = [ alliance coriolis-unicorn yosys ];
 
   YOSYS_TOP = "${yosys}";
+  ALLIANCE_TOP = "${alliance}";
+  CORIOLIS_TOP = "${coriolis-unicorn}";
+  # The user configuration for verhaegs is empty,
+  # which is why we use it.
+  USER = "verhaegs";
 
   configurePhase = "true";
   buildPhase = ''
