c6624909de29736a1693d4f76acba5c55ffd9c9b
Las Safin <me@las.rs>
2021-09-03 13:46:33 +0000

    Keep backward compatibility for cumulus users

--- nix/cumulus.nix
+++ nix/cumulus.nix
@@ -3,13 +3,15 @@
 let pkg =
   { lib, coriolis-vlsisapd, coriolis-hurricane, coriolis-crlcore
   , coriolis-etesian, coriolis-anabatic, coriolis-katana
-  , coriolis-unicorn }:
+  , coriolis-unicorn, python2Packages }:
   {
     name = "cumulus";
 
     src = ../cumulus;
 
     postInstall = ''
+      ln -s -t $out/${python2Packages.python.sitePackages} $out/${python2Packages.python.sitePackages}/cumulus/*
+
       # for import check
       mkdir -p /build/coriolistop/etc/coriolis2
       export CORIOLIS_TOP=/build/coriolistop
