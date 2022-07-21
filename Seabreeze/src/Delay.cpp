bf181787cb9721624aa6dec86699baaf21adc394
HoangAnhP <vu.pham@polytechnique.edu>
2022-07-21 12:03:52 +0200

    Seabreeze : new class Delay for the result

--- Seabreeze/src/Delay.cpp
+++ Seabreeze/src/Delay.cpp
@@ -18,6 +18,7 @@
 #include <map>
 #include <iostream>
 #include "hurricane/RoutingPad.h"
+#include "seabreeze/Delay.h"
 
 using namespace std;
 
@@ -28,18 +29,23 @@ namespace Seabreeze {
 //---------------------------------------------------------
 // Class : Seabreeze::Delay
 
-  class Delay {
-    public:
-                                                               Delay       ();
-                                                              ~Delay       ();
-      inline const map<RoutingPad*, map<RoutingPad*, double>>& getValues   () const ;
-                   void                                        addPair     ( RoutingPad*, RoutingPad*, double );
-                   void                                        addValue    ( RoutingPad*, RoutingPad*, double );
-                   void                                        printDelays ();
-      
-    private:
-      map<RoutingPad*, map<RoutingPad*, double>> _values;
-  };
-  
-    inline const map<RoutingPad*, map<RoutingPad*, double>>& Delay::getValues () const { return _values; }
+    Delay::Delay()
+      : _values()
+    {}
+
+    Delay::~Delay()
+    {}
+    
+    void Delay::addPair ( RoutingPad* driver, RoutingPad* sink, double delay)
+    { 
+      if ( _values.count(driver) > 0 ) {
+        cerr << "Driver already exist." << endl
+             << "If you want to add value, please use the function addValue( RoutingPad*, RoutingPad*, double)" << endl
+             << "Example : addValue(driver, sink, delay)" << endl;
+      }
+      else {
+        map<RoutingPad*, double> val { {sink, delay}, };
+        _values.insert(driver, val);
+      }
+    }
 }
