dedad34585b80e8fce2bd25e9f93155e45ccf742
HoangAnhP <vu.pham@polytechnique.edu>
2022-07-22 12:39:08 +0200

    Complete fonctions of class Delay

--- Seabreeze/src/Delay.cpp
+++ Seabreeze/src/Delay.cpp
@@ -14,7 +14,6 @@
 // +-----------------------------------------------------------------+
 
 
-#pragma once
 #include <map>
 #include <iostream>
 #include "hurricane/RoutingPad.h"
@@ -39,13 +38,34 @@ namespace Seabreeze {
     void Delay::addPair ( RoutingPad* driver, RoutingPad* sink, double delay)
     { 
       if ( _values.count(driver) > 0 ) {
-        cerr << "Driver already exist." << endl
+        cerr << "Driver " << driver << " already exists." << endl
              << "If you want to add value, please use the function addValue( RoutingPad*, RoutingPad*, double)" << endl
              << "Example : addValue(driver, sink, delay)" << endl;
       }
       else {
         map<RoutingPad*, double> val { {sink, delay}, };
-        _values.insert(driver, val);
+        _values.insert({driver, val});
+      }
+    }
+
+    void Delay::addValue ( RoutingPad* driver, RoutingPad* sink, double delay )
+    {
+      if ( _values[driver].count(sink) > 0 ) {
+        cerr << "Delay from " << driver << " to " << sink << " is already computed" << endl;
+      }
+      else {
+        _values[driver].insert({sink, delay});
+      }
+    }
+
+    void Delay::printDelays ()
+    {
+      for ( auto dmap : _values ) {
+        cerr << "Delay from : " << dmap.first << " to  :" << endl;
+        map<RoutingPad*, double>& val = dmap.second;
+        for ( auto smap : val ) {
+          cerr << smap.first << " : " << smap.second << endl;
+        }
       }
     }
 }
