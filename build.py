f8afd5bdede476ee21edf02263a7669c7a16371b
Robert Taylor <rob.taylor@chipflow.io>
2022-11-26 02:41:26 +0000

    Move to packaging requiring no changes to CMake

--- build.py
+++ build.py
@@ -48,7 +48,7 @@ class ExtensionBuilder(build_ext):
 
     def build_cmake_extension(self, ext: CMakeExtension) -> None:
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
-        cmake_args = ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=" + extdir, "-DPYTHON_EXECUTABLE=" + sys.executable]
+        cmake_args = []
 
         cfg = "Debug" if self.debug else "Release"
         # cfg = 'Debug'
@@ -56,7 +56,6 @@ class ExtensionBuilder(build_ext):
         install_args = ["--config", cfg]
 
         if platform.system() == "Windows":
-            cmake_args += ["-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}".format(cfg.upper(), extdir)]
             if sys.maxsize > 2 ** 32:
                 cmake_args += ["-A", "x64"]
             build_args += ["--", "/m"]
@@ -69,25 +68,48 @@ class ExtensionBuilder(build_ext):
         env["CXXFLAGS"] = '{} -DVERSION_INFO=\\"{}\\"'.format(env.get("CXXFLAGS", ""), self.distribution.get_version())
 
         build_dir = os.path.join(self.build_temp, ext.sourcedir_rel)
-        install_dir = os.path.join(self.build_temp, "install")
         os.makedirs(build_dir,exist_ok=True)
-        os.makedirs(os.path.join(install_dir, cfg), exist_ok=True)
 
         cmake_args += [f"-DCMAKE_MODULE_PATH={os.path.abspath('bootstrap/cmake_modules')};"
-                       f"{os.path.join(install_dir, 'share/cmake/Modules')}"]
-        cmake_args += [f"-DCMAKE_INSTALL_PREFIX={install_dir}"]
+                       f"{os.path.join(extdir, 'share/cmake/Modules')}"]
+        cmake_args += [f"-DCMAKE_INSTALL_PREFIX={extdir}"]
         cmake_args += [f"-DPython_CORIOLISLIB={extdir}"]
         cmake_args += [f"-DPython_CORIOLISARCH={extdir}"]
+        cmake_args += [f"-DSYS_CONF_DIR={extdir}"]
+        cmake_args += [f"-DCORIOLIS_TOP={extdir}"]
+        cmake_args += [f"-DCORIOLIS_USER_TOP={extdir}"]
+        cmake_args += [f"-DPOETRY=1"]
+        cmake_args += [f"-DWITH_QT5=1"]
     
 
         subprocess.check_call(["cmake", "--debug-find", "--trace-redirect=build.cmake.trace", "--trace-expand",  ext.sourcedir] + cmake_args, cwd=build_dir, env=env)
         subprocess.check_call(["cmake", "--build", "."] + build_args, cwd=build_dir)
-        subprocess.check_call(["cmake", "--install", ".", "--prefix", install_dir] + install_args, cwd=build_dir)
+        subprocess.check_call(["cmake", "--install", ".", "--prefix", extdir] + install_args, cwd=build_dir)
 
 
 def build(setup_kwargs: Dict[str, Any]) -> None:
-    cmake_modules = [CMakeExtension("coriolis.hurricane", sourcedir="coriolis/hurricane"),
-                     CMakeExtension("coriolis.crlcore", sourcedir="coriolis/crlcore")]
+    cmake_modules = [
+                     CMakeExtension("Coriolis.coloquinte", sourcedir="coloquinte"),
+                     CMakeExtension("Coriolis.hurricane", sourcedir="hurricane"),
+                     CMakeExtension("Coriolis.crlcore", sourcedir="crlcore"),
+                     CMakeExtension("Coriolis.flute", sourcedir="flute"),
+                     CMakeExtension("Coriolis.etesian", sourcedir="etesian"),
+                     CMakeExtension("Coriolis.anabatic", sourcedir="anabatic"),
+                     CMakeExtension("Coriolis.katana", sourcedir="katana"),
+                     CMakeExtension("Coriolis.equinox", sourcedir="equinox"),
+                     CMakeExtension("Coriolis.solstice", sourcedir="solstice"),
+                     CMakeExtension("Coriolis.oroshi", sourcedir="oroshi"),
+                     CMakeExtension("Coriolis.bora", sourcedir="bora"),
+                     CMakeExtension("Coriolis.karakaze", sourcedir="karakaze"),
+                     CMakeExtension("Coriolis.unicorn", sourcedir="unicorn"),
+                     CMakeExtension("Coriolis.tutorial", sourcedir="tutorial"),
+                     CMakeExtension("Coriolis.ispd", sourcedir="ispd"),
+                     CMakeExtension("Coriolis.cumulus", sourcedir="cumulus"),
+                     CMakeExtension("Coriolis.stratus1", sourcedir="stratus1"),
+                     CMakeExtension("Coriolis.documentation", sourcedir="documentation"),
+                     CMakeExtension("Coriolis.unittests", sourcedir="unittests")
+                     ]
+ 
     ext_modules = cmake_modules
     setup_kwargs.update(
         {
