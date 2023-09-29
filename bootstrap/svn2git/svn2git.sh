b2eda2598a266a2113e367ee1d15b7d48e5943e7
Rob Taylor <rob.taylor@chipflow.io>
2023-09-29 14:53:58 +0100

    Meson: remove old build system artifacts
    This has the effect of removing nix build as well, which is not ported
    to meson.

--- bootstrap/svn2git/svn2git.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
- svn-all-fast-export --resume-from  0                \
-                     --rules        ./coriolis.rules \
-                     --identity-map ./authors.txt    \
-                     /dsk/l1/jpc/svn2git/svn         > svn-all-fast-export.log
