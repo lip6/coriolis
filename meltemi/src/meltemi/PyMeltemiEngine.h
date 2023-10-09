aaf42b5a65f89a314cd49f4eefdeee988b324a25
Gabriel Gouvine <gabriel.gouvine_git@m4x.org>
2023-10-09 18:43:17 +0200

    Compile a Python MeltemiEngine

---
+++ meltemi/src/meltemi/PyMeltemiEngine.h
@@ -0,0 +1,50 @@
+
+// -*- C++ -*-
+//
+// This file is part of the Coriolis Software.
+// Copyright (c) SU/LIP6 2014-2020, All Rights Reserved
+//
+// +-----------------------------------------------------------------+ 
+// |                   C O R I O L I S                               |
+// |   E t e s i a n  -  A n a l y t i c   P l a c e r               |
+// |                                                                 |
+// |  Author      :                    Jean-Paul CHAPUT              |
+// |  E-mail      :            Jean-Paul.Chaput@lip6.fr              |
+// | =============================================================== |
+// |  C++ Header  :  "./meltemi/PyMeltemiEngine.cpp"                 |
+// +-----------------------------------------------------------------+
+
+
+#pragma once
+#include "hurricane/isobar/PyHurricane.h"
+#include "crlcore/PyToolEngine.h"
+#include "meltemi/PyMeltemiEngine.h"
+
+
+namespace  Meltemi {
+
+extern "C" {
+
+
+// -------------------------------------------------------------------
+// Python Object  :  "PyMeltemiEngine".
+
+  typedef struct {
+      CRL::PyToolEngine  _baseObject;
+  } PyMeltemiEngine;
+
+
+// -------------------------------------------------------------------
+// Functions & Types exported to "PyMeltemi.ccp".
+
+  extern  PyTypeObject  PyTypeMeltemiEngine;
+  extern  PyMethodDef   PyMeltemiEngine_Methods[];
+
+  extern  PyObject* PyMeltemiEngine_Link           ( Meltemi::MeltemiEngine* );
+  extern  void      PyMeltemiEngine_LinkPyType     ();
+  extern  void      PyMeltemiEngine_postModuleInit ();
+
+
+}  // extern "C".
+
+}  // Meltemi namespace.
