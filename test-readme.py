096d7b5cbe15dd53cdaf949d80a4656ce1a2f0d2
Rob Taylor <rob.taylor@chipflow.io>
2023-10-16 13:14:44 +0100

    Make test-readme less verbose and prettier

--- test-readme.py
+++ test-readme.py
@@ -1,21 +1,24 @@
 #!/bin/env python3
-import docutils.utils
-import tempfile
 import bashlex
-import asyncio
+import docutils.utils
 import logging
+import re
+import tempfile
 
-from docutils.parsers.rst import Parser
+from asyncio import create_subprocess_exec, run
+from asyncio.subprocess import PIPE
 from docutils.frontend import get_default_settings
+from docutils.parsers.rst import Parser
 from docutils.nodes import NodeVisitor
 
-#logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
+
 
 def parse_rst(text: str) -> docutils.nodes.document:
     parser = Parser()
     settings = get_default_settings(Parser)
     document = docutils.utils.new_document('<rst-doc>', settings=settings)
-    #document.reporter.debug_flag = True
+    # document.reporter.debug_flag = True
     parser.parse(text, document)
     return document
 
@@ -41,7 +44,8 @@ class MyVisitor(NodeVisitor):
 
 
 commands = []
-with open("README.rst") as f:
+filename = "README.rst"
+with open(filename) as f:
     s = f.read()
     doc = parse_rst(s)
     visitor = MyVisitor(doc)
@@ -51,22 +55,21 @@ with open("README.rst") as f:
         parts = bashlex.parse(node.rawsource)
         for n in parts:
             command = node.rawsource[n.pos[0]:n.pos[1]]
-            commands.append((node.line,command))
+            commands.append((node.line, command))
 
-
-import re
 
 debug = False
 
+
 async def run_commands(commands):
-    stdout_barrier = re.compile("^============= RSTTESTER RETURN ([0-9]+) ============$")
-    stderr_barrier  = re.compile("^============= RSTTESTER ERROR COMPLETE ============$")
+    stdout_barrier = re.compile("^====== RSTTESTER RETURN ([0-9]+) ======$")
+    stderr_barrier = re.compile("^====== RSTTESTER ERROR COMPLETE ======$")
     with tempfile.TemporaryDirectory() as d:
-        process = await asyncio.create_subprocess_exec('/bin/bash',
-                                stdin=asyncio.subprocess.PIPE,
-                                stdout=asyncio.subprocess.PIPE,
-                                stderr=asyncio.subprocess.PIPE,
-                                cwd=d)
+        process = await create_subprocess_exec('/bin/bash',
+                                               stdin=PIPE,
+                                               stdout=PIPE,
+                                               stderr=PIPE,
+                                               cwd=d)
 
         def write(s):
             process.stdin.write(s.encode('utf-8'))
@@ -74,38 +77,41 @@ async def run_commands(commands):
 
         logging.info(f'Running:\n{commands}')
         for cmd in commands:
-            logging.info(f"---- line: {cmd[0]}\n{cmd[1]}\n\n")
+            print(f"- Running line {cmd[0]} of {filename}: '{cmd[1]}'")
             write(cmd[1])
-            write('\necho "============= RSTTESTER RETURN $? ============"\n')
+            write('\necho "====== RSTTESTER RETURN $? ======"\n')
             lines = []
             line = ""
             while stdout_barrier.match(line) is None:
+                lines.append(line) if line != "" else None
                 data = await process.stdout.readline()
                 line = data.decode('utf-8').rstrip()
                 logging.debug(f'> {line}')
-                lines.append(line)
 
-            logging.debug (stdout_barrier.match(line))
+            logging.debug(stdout_barrier.match(line))
             exitcode = int(stdout_barrier.match(line).group(1))
 
-
-            write('\necho "============= RSTTESTER ERROR COMPLETE ============" 1>&2\n')
+            write('\necho "====== RSTTESTER ERROR COMPLETE ======" 1>&2\n')
             errlines = []
             line = ""
             while stderr_barrier.match(line) is None:
+                errlines.append(line) if line != "" else None
                 data = await process.stderr.readline()
                 line = data.decode('utf-8').rstrip()
                 logging.debug(f'E {line}')
-                errlines.append(line)
 
             if (exitcode > 0):
-                logging.error(f"Command failed at line {cmd[0]}: \n{cmd[1]}\n")
+                print(f"- Command failed at line {cmd[0]}: '{cmd[1]}'")
+                print("- stdout:")
+                print('\n'.join(lines))
+                print("- stderr:")
+                print('\n'.join(errlines))
 
             logging.info(f"---- returned {exitcode}")
             logging.info(f"---- stdout:\n{lines}")
             logging.info(f"---- stdout:\n{errlines}")
 
-asyncio.run(run_commands(commands))
+run(run_commands(commands))
 
 # with tempfile.TemporaryDirectory() as d:
 #    out, err = process.communicate(commands.encode('utf-8'))
