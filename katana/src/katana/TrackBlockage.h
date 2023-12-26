8868b50197a83f2f898b0832a6c0ae261abd854c
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2023-12-26 15:47:49 +0100

    Create a new type of TrakElement : TrackBlockage.
    * TrackBlockage do not rely on an existing AutoSegment, or even a
      Segment, it is a portion of Track that we declare as blocked.
    * Used to manage the blockages coming for the LocalOnly planes,
      typically terminal planes. As it is not related to a Segment,
      we do not need to flatten the obstacles coming from the
      standard cells (would be a lot of them).
    * In PowerRails, also support blockages from Rectilinear,
      decomposed in vertical rectangles.
    * In Session, add a mechanism for TrackElement that have been put
      in their track *before* closing the session (used for
      TrackBlockage).

---
+++ katana/src/katana/TrackBlockage.h
@@ -0,0 +1,87 @@
+// -*- C++ -*-
+//
+// This file is part of the Coriolis Software.
+// Copyright (c) UPMC 2008-2018, All Rights Reserved
+//
+// +-----------------------------------------------------------------+
+// |                   C O R I O L I S                               |
+// |      K i t e  -  D e t a i l e d   R o u t e r                  |
+// |                                                                 |
+// |  Author      :                    Jean-Paul CHAPUT              |
+// |  E-mail      :       Jean-Paul.Chaput@asim.lip6.fr              |
+// | =============================================================== |
+// |  C++ Header  :  "./katana/TrackBlockage.h"                      |
+// +-----------------------------------------------------------------+
+
+
+#pragma  once
+#include "katana/TrackElement.h"
+
+
+namespace Katana {
+
+  using std::string;
+  using std::map;
+  using Hurricane::Record;
+  using Hurricane::Interval;
+  using Hurricane::DbU;
+  using Hurricane::Net;
+  using Hurricane::Layer;
+
+  class Track;
+
+
+// -------------------------------------------------------------------
+// Class  :  "TrackBlockage".
+
+  class TrackBlockage : public TrackElement {
+    public:
+      static  TrackElement*  create                 ( Katana::Track*, Box );
+    public:                                         
+      virtual AutoSegment*   base                   () const;
+      virtual Segment*       getSegment             () const;
+      virtual bool           isHorizontal           () const;
+      virtual bool           isVertical             () const;
+      virtual bool           isFixed                () const;
+      virtual bool           isPriorityLocked       () const;
+      virtual unsigned long  getId                  () const;
+      virtual Flags          getDirection           () const;
+      virtual Net*           getNet                 () const;
+      virtual DbU::Unit      getWidth               () const;
+      virtual unsigned int   getDepth               () const;
+      virtual const Layer*   getLayer               () const;
+      virtual size_t         getTrackSpan           () const;
+      virtual TrackElement*  getNext                () const;
+      virtual TrackElement*  getPrevious            () const;
+      virtual DbU::Unit      getAxis                () const;
+      inline  DbU::Unit      getSourceAxis          () const;
+      virtual DbU::Unit      getTargetAxis          () const;
+      virtual Interval       getFreeInterval        () const;
+      virtual void           addOverlapCost         ( TrackCost& ) const;
+      virtual float          getPriority            () const;
+      virtual void           setPriorityLock        ( bool );
+      virtual void           forcePriority          ( float );
+      virtual void           computePriority        ();
+      virtual void           computeAlignedPriority ();
+      virtual void           detach                 ( TrackSet& );
+      virtual Record*        _getRecord             () const;
+      virtual string         _getString             () const;
+      virtual string         _getTypeName           () const;
+    private:
+              Box  _boundingBox;
+    protected:
+    // Constructors & Destructors.
+                                 TrackBlockage     ( Track*, Box ) ;
+      virtual                   ~TrackBlockage     ();
+      virtual void               _postCreate       ();
+      virtual void               _preDestroy       ();
+    private:
+                                 TrackBlockage ( const TrackBlockage& ) = delete;
+              TrackBlockage& operator=         ( const TrackBlockage& ) = delete;
+  };
+
+
+}  // Katana namespace.
+
+
+INSPECTOR_P_SUPPORT(Katana::TrackBlockage);
