c83a3cabc9c56ad42cbb2fb419d355bebf4595f0
Serge Rabyking <serge.rabyking@chipflow.io>
2024-03-01 23:34:52 +0000

    Fixed horizontal cells alignment in several places    (height was used before)

--- cumulus/src/plugins/chip/chip.py
+++ cumulus/src/plugins/chip/chip.py
@@ -120,8 +120,8 @@ class Chip ( Block ):
             x = (coronaAb.getWidth () - self.conf.coreAb.getWidth ()) // 2
             y = (coronaAb.getHeight() - self.conf.coreAb.getHeight()) // 2
             trace( 550, '\tCore X, {} '.format(DbU.getValueString(x)) )
-            x = x - (x % self.conf.sliceHeight)
-            trace( 550, ' adjusted on {}, {}\n'.format( DbU.getValueString(self.conf.sliceHeight)
+            x = x - (x % self.conf.sliceStep)
+            trace( 550, ' adjusted on {}, {}\n'.format( DbU.getValueString(self.conf.sliceStep)
                                                       , DbU.getValueString(x)) )
             y = y - (y % self.conf.sliceHeight)
             self.conf.icore.setTransformation ( Transformation(x,y,Transformation.Orientation.ID) )
