2b36c0ce45e27cb447d5c6a2361fa84f17c4c5a8
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2024-05-18 18:39:13 +0200

    Add S2R & DRC through KLayout in pnrcheck rules set.

--- cumulus/src/designflow/pnrcheck.py
+++ cumulus/src/designflow/pnrcheck.py
@@ -6,6 +6,10 @@ from .cougar   import Cougar
 from .lvx      import Lvx
 from .druc     import Druc
 from .pnr      import PnR
+from .s2r      import S2R
+from .klayout  import DRC
+from .graal    import Graal
+from .dreal    import Dreal
 from .alias    import Alias
 from .clean    import Clean
 PnR.textMode = True
@@ -13,7 +17,8 @@ PnR.textMode = True
 
 UseClockTree = 0x0001
 NoSynthesis  = 0x0002
-IsChip       = 0x0004
+NoGDS        = 0x0004
+IsChip       = 0x0008
 
 
 def mkRuleSet ( callerGlobals, vlogDesignName, flags=0, extraRtlDepends=[], extrasClean=[] ):
@@ -52,9 +57,18 @@ def mkRuleSet ( callerGlobals, vlogDesignName, flags=0, extraRtlDepends=[], extr
     ruleDruc   = Druc  .mkRule( 'druc'  , [rulePnR], flags=0 )
     ruleLayout = Alias .mkRule( 'layout', [rulePnR] )
     ruleCgt    = PnR   .mkRule( 'cgt' )
+    ruleGraal  = Graal .mkRule( 'graal', rulePnR.file_target(0) )
+
+    if not (flags & NoGDS):
+        ruleGds   = S2R   .mkRule( 'gds', rulePnR.file_target(0).with_suffix('.gds'), rulePnR.file_target(0)
+                                 , flags=S2R.Verbose|S2R.NoReplaceBlackboxes )
+        ruleDRC   = DRC   .mkRule( 'drc'  , ruleGds.file_target(0) )
+        ruleDreal = Dreal .mkRule( 'dreal', ruleGds.file_target(0) )
+
     ruleClean  = Clean .mkRule( extrasClean )
 
-    for tag in [ 'Yosys', 'B2V', 'PnR', 'Cougar', 'Lvx', 'Druc', 'Cgt', 'Layout', 'Clean' ]:
+    for tag in [ 'Yosys', 'B2V', 'PnR', 'Cougar', 'Lvx', 'Druc', 'Cgt'
+               , 'Layout', 'Gds', 'DRC', 'Graal', 'Dreal', 'Clean' ]:
         rule = 'rule' + tag
         if rule in locals():
             callerGlobals[ rule ] = locals()[ rule ]
