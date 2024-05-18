f3dd50b31e9491c3612e0c67a44984e06f14b93d
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2024-05-18 18:39:13 +0200

    Add S2R & DRC through KLayout in pnrcheck rules set.

--- cumulus/src/designflow/klayout.py
+++ cumulus/src/designflow/klayout.py
@@ -7,6 +7,7 @@ from   .task import FlowTask, ShellEnv
 
 
 class MissingTarget ( Exception ): pass
+class BadDrcRules   ( Exception ): pass
 
 
 class Klayout ( FlowTask ):
@@ -26,7 +27,7 @@ class Klayout ( FlowTask ):
         for name, value in variables.items():
             self.command += [ '-rd', '{}={}'.format(name,value) ]
         self.command += [ '-b', '-r', self.file_depend(0).as_posix() ]
-       #self.addClean( self.targets )
+        self.addClean( self.targets )
 
     def __repr__ ( self ):
         return '<{}>'.format( ' '.join(self.command) )
@@ -48,4 +49,37 @@ class Klayout ( FlowTask ):
                , 'targets'  : self.targets
                , 'file_dep' : self.file_dep
                }
+
+
+class DRC ( Klayout ):
+
+    _drcRules = None
+
+    @staticmethod
+    def setDrcRules ( rules ):
+        if   isinstance(rules,Path): pass
+        elif isinstance(rules,str):  rules = Path( rules )
+        else:
+            raise BadDrcRules( '[ERROR] DRC.setDrcRules(): Should be <str> or <Path> ({})' \
+                               .format( rules ))
+        if not rules.is_file():
+            raise BadDrcRules( '[ERROR] DRC.setDrcRules(): File not found "{}"' \
+                               .format( rules ))
+        DRC._drcRules = rules
+
+    @staticmethod
+    def mkRule ( rule, depends=[], flags=0 ):
+        return DRC( rule, depends, flags )
+
+    def __init__ ( self, rule, depends, flags ):
+        from ..helpers.io import ErrorMessage
+
+        depends = FlowTask._normFileList( depends )
+        targets = [ depends[0].with_suffix('.kdrc-report.txt') ]
+        if not DRC._drcRules:
+            raise ErrorMessage( 1, 'DRC.doTask(): No DRC rules defined.' )
+        variables = { 'input'  : depends[0]
+                    , 'report' : targets[0]
+                    }
+        super().__init__( rule, targets, depends, DRC._drcRules, variables, flags )
         
