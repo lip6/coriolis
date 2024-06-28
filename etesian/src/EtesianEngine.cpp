5924f58fc5445b4b75522dfcbbc123845d9241ee
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2024-06-28 17:29:18 +0200

    Fix erroneous latch-up cell insertion when there is not enough free space.
    * Bug: In SubSlice::getAverageChunk(), when there is no space avalaible,
        the function must return an "infinite" distance (DbU::Max) instead
        of 0. As this number is the average distance between two latch-up
        cells, it was meaning the insertion was possible.
          Inserting those extra cells was leading to a ragged left vertical
        edge and terminal outside the PnR areas (cascading errors)...

--- etesian/src/EtesianEngine.cpp
+++ etesian/src/EtesianEngine.cpp
@@ -1211,6 +1211,7 @@ namespace Etesian {
     UpdateSession::close();
   }
 
+
   void  EtesianEngine::_updatePlacement ( const coloquinte::PlacementSolution* placement, uint32_t flags )
   {
     UpdateSession::open();
