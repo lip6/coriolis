7bab735860a40885cc95c0bc0042914ea0f83dc3
Jean-Paul Chaput <Jean-Paul.Chaput@lip6.fr>
2025-02-10 21:49:26 +0100

    Make the version.py return something (2.5.5) even if not under git.

--- _build_utils/version.py
+++ _build_utils/version.py
@@ -1,15 +1,28 @@
 #!/usr/bin/env python3
 
-def get_version():
-    from dunamai import Version
-    version = Version.from_git()
+from pathlib import Path
 
-    if version.distance > 0:
-        version.stage = "a"
-        version.revision = version.distance
-        version.distance = 0
-    return version.serialize()
+def get_version():
+    moduleDir   = Path( __file__ ).parent
+    versionFile = moduleDir / 'gitversion.txt'
+    try:
+        from dunamai import Version
+        version = Version.from_git()
+        
+        if version.distance > 0:
+            version.stage    = "a"
+            version.revision = version.distance
+            version.distance = 0
+        with versionFile.open( 'w' ) as fd:
+            fd.write( version.serialize() )
+        return version.serialize()
+    except RuntimeError:
+        if versionFile.is_file():
+            with versionFile.open( 'r' ) as fd:
+                version = fd.readline()
+                return version
+        return '2.5.5'
 
 
 if __name__ == "__main__":
-    print(get_version())
+    print( get_version() )
